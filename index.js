fe81cf190326568a1ec21c4cc959efa7033cc84f
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-07 15:01:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(line, name) {
   line.push(name);
-  return `Welcome, ${name}. You are number ${line.length}.`;
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
 function nowServing(line) {
